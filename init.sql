56ea18af7a6faf95609da995a87651b5a6b61059
Priyansh Verma <priyansh.verma1010@gmail.com>
2026-01-11 07:06:06 +0530

    Deploy setup

--- init.sql
+++ init.sql
@@ -3,21 +3,72 @@
 
 USE exam_system;
 
-CREATE TABLE `answers` (
-  `attempt_id` bigint NOT NULL,
+-- 1. Create Independent Tables first
+
+-- exam_system.users definition
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `uq_users_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 2. Create Tables validating against Users
+
+-- exam_system.exams definition
+CREATE TABLE `exams` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `description` text,
+  `duration` int DEFAULT NULL,
+  `total_marks` int NOT NULL,
+  `created_by` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(50) NOT NULL DEFAULT 'DRAFT',
+  PRIMARY KEY (`id`),
+  KEY `fk_exam_admin` (`created_by`),
+  CONSTRAINT `fk_exam_admin` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `chk_exam_status` CHECK ((`status` in (_utf8mb4'DRAFT',_utf8mb4'PUBLISHED')))
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 3. Create Tables validating against Exams
+
+-- exam_system.questions definition
+CREATE TABLE `questions` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `exam_id` bigint NOT NULL,
+  `question_text` text NOT NULL,
+  `marks` int DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_questions_exam` (`exam_id`),
+  CONSTRAINT `fk_question_exam` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 4. Create Tables validating against Questions
+
+-- exam_system.`options` definition
+CREATE TABLE `options` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `question_id` bigint NOT NULL,
-  `selected_option_id` bigint NOT NULL,
-  PRIMARY KEY (`attempt_id`,`question_id`),
-  KEY `fk_answer_question` (`question_id`),
-  KEY `fk_answer_option` (`selected_option_id`),
-  KEY `idx_answers_attempt` (`attempt_id`),
-  CONSTRAINT `fk_answer_attempt` FOREIGN KEY (`attempt_id`) REFERENCES `attempts` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_answer_option` FOREIGN KEY (`selected_option_id`) REFERENCES `options` (`id`),
-  CONSTRAINT `fk_answer_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `option_label` char(1) NOT NULL,
+  `option_text` varchar(255) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_options_question` (`question_id`),
+  CONSTRAINT `fk_option_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=226 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 5. Create Tables validating against Users and Exams
 
 -- exam_system.attempts definition
-
 CREATE TABLE `attempts` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
@@ -34,61 +85,17 @@ CREATE TABLE `attempts` (
   CONSTRAINT `fk_attempt_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- exam_system.exams definition
+-- 6. Create Tables validating against Attempts, Questions, and Options
 
-CREATE TABLE `exams` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) NOT NULL,
-  `description` text,
-  `duration` int DEFAULT NULL,
-  `total_marks` int NOT NULL,
-  `created_by` bigint NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(50) NOT NULL DEFAULT 'DRAFT',
-  PRIMARY KEY (`id`),
-  KEY `fk_exam_admin` (`created_by`),
-  CONSTRAINT `fk_exam_admin` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `chk_exam_status` CHECK ((`status` in (_utf8mb4'DRAFT',_utf8mb4'PUBLISHED')))
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- exam_system.`options` definition
-
-CREATE TABLE `options` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+CREATE TABLE `answers` (
+  `attempt_id` bigint NOT NULL,
   `question_id` bigint NOT NULL,
-  `option_label` char(1) NOT NULL,
-  `option_text` varchar(255) NOT NULL,
-  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `idx_options_question` (`question_id`),
-  CONSTRAINT `fk_option_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=226 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- exam_system.questions definition
-
-CREATE TABLE `questions` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `exam_id` bigint NOT NULL,
-  `question_text` text NOT NULL,
-  `marks` int DEFAULT '1',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `idx_questions_exam` (`exam_id`),
-  CONSTRAINT `fk_question_exam` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- exam_system.users definition
-
-CREATE TABLE `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(50) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `uq_users_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `selected_option_id` bigint NOT NULL,
+  PRIMARY KEY (`attempt_id`,`question_id`),
+  KEY `fk_answer_question` (`question_id`),
+  KEY `fk_answer_option` (`selected_option_id`),
+  KEY `idx_answers_attempt` (`attempt_id`),
+  CONSTRAINT `fk_answer_attempt` FOREIGN KEY (`attempt_id`) REFERENCES `attempts` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_answer_option` FOREIGN KEY (`selected_option_id`) REFERENCES `options` (`id`),
+  CONSTRAINT `fk_answer_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
